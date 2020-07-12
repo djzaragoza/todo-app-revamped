a34cd40c275498a6c07f6c85fa4c6b636e9609fd
unknown <dj.zaragoza@icloud.com>
2020-07-11 22:37:26 -0700

    testing and deploying for display

--- app.js
+++ app.js
@@ -24,11 +24,13 @@ function addToDo(event) {
    // check mark button
    const completedButton = document.createElement('button');
    completedButton.innerHTML = '<i class="fas fa-check"> </i>';
-   completedButton.classList.add('completed-btn');
+   completedButton.classList.add('complete-btn');
    todoDiv.appendChild(completedButton);
    // check trash button
    const trashButton = document.createElement('button');
-   trashButton.innerHTML = '<i class="fas fa-check"> </i>';
-   trashButton.classList.add('completed-btn');
+   trashButton.innerHTML = '<i class="fas fa-trash"> </i>';
+   trashButton.classList.add('trash-btn');
    todoDiv.appendChild(trashButton);
+   // append to list
+   todoList.appendChild(todoDiv);
 }
