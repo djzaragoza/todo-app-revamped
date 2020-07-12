3e14e31684b066030758b5656d971ae03efd345b
unknown <dj.zaragoza@icloud.com>
2020-07-11 22:48:26 -0700

    adding todo clear field input value and padding for text in input field

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ function addToDo(event) {
    todoDiv.classList.add('todo');
    // create Li
    const newTodo = document.createElement('li');
-   newTodo.innerText = 'hey';
+   newTodo.innerText = todoInput.value;
    newTodo.classList.add('todo-item');
    todoDiv.appendChild(newTodo);
    // check mark button
@@ -33,4 +33,6 @@ function addToDo(event) {
    todoDiv.appendChild(trashButton);
    // append to list
    todoList.appendChild(todoDiv);
+   // clear todo input value
+   todoInput.value = '';
 }
